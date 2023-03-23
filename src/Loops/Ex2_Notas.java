5add69ffa91e0e7bdecfea58ac5c08542b651f3e
Natalia - ntlcs <nataliadacostasilva@gmail.com>
2023-03-23 13:38:57 -0300

    first commit

---
+++ src/Loops/Ex2_Notas.java
@@ -0,0 +1,22 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class Ex2_Notas {
+ 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Nota: ");
+        nota = scan.nextInt();
+
+        while(nota < 0 | nota > 10) {
+            System.out.println("Nota Inválida! Digite novamente: ");
+            nota = scan.nextInt();
+
+        }
+
+    }
+}
